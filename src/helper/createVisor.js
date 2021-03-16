708fd7f70ffd9e357fc01bbbd349119a5c8d614a
jackyx-zhou <jackyx.zhou@gmail.com>
2021-03-16 03:42:15 +0000

    Fix visor responsiveness on mobile device

--- src/helper/createVisor.js
+++ src/helper/createVisor.js
@@ -4,5 +4,6 @@ export default function createVisor() {
     const visor = tfvis.visor();
     visor.el.style.position = "absolute";
     visor.el.style.zIndex = 2000;
+    visor.el.children[0].style.maxWidth="100%";
     visor.close();
 }
